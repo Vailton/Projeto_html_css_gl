07d331a52e6218705c1b65b4159a253c853503c1
Vailton_Gomes <vailton.tech@gamil.com>
2020-04-05 16:37:05 -0300

    Atualizar

---
+++ app.js
@@ -0,0 +1,26 @@
+'use strict'
+
+let today = new Date()
+let formatDate = today.toDateString()
+let selectElement = document.getElementById('date')
+selectElement.innerHTML = formatDate
+
+console.log('Here\'s a hidden message')
+
+
+
+
+
+
+
+
+
+
+
+// Comentário simples.
+
+/* Comentário
+várias linhas
+ou grandes. */
+
+// console.log('This code is no longer active')
